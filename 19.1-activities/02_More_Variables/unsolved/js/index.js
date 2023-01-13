751fccc8a2718b9f6a98b19d37c6de0f2aeba0e9
songyip92 <park.melina@gmail.com>
2023-01-12 19:11:53 -0500

    19

---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,23 @@
+// Create the addNumbers function here.
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+
+    var two = two + three;
+    
+    console.log(five);
+}
+
+// Create the stringConcat function here.
+function stringConcat() {
+   
+    var taco= 'taco';
+    var cat= 'cat';
+
+    var tacoCat = 'taco' + ' ' + 'cat';
+
+alert(tacoCat);
+
+}
+
+stringConcat();
---
+++ 19.1-activities/05_Build_a_toggle/unsolved/js/index.js
@@ -0,0 +1,5 @@
+// Add Event Listener Below
+$("#toggle").on("click", function(){
+    $("#dropDown").slideToggle();
+
+});
